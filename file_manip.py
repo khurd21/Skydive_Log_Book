7e4d4836bed8dc29d453b62611c35a347205935c
khurd21 <kyle.hurd@wsu.edu>
2020-12-27 22:07:43 -0800

    Added docstrings

--- file_manip.py
+++ file_manip.py
@@ -49,13 +49,35 @@ def get_info_data(file_name):
         return items
         
 
-def save_info_data(file_name):
-    ''' Not complete
+def save_info_file(file_name, info_list):
+    ''' Writes user's specs to the info.txt
+    
+    :param file_name: the full path to the .txt file
+           info_list: the user's specs to be written
+    :type file_name:  string
+          info_list:  list of strings
+    :rtype: N/A
     '''
-    pass
+    with open(file_name, 'w' as outfile):
+        print(f'name: {info_list[0]}')
+        print(f'filename: {info_list[1]}')
+        print(f'current parachute: {info_list[2]}')
+        print(f'DOM: {info_list[3]')
+        print(f'weight [lbs]: {info_list[4]}')
+        print(f'parachute size [sq. ft.]: {info_list[5]}')
+        
+    return
     
     
 def save_csv_file(file_name, csv_list):
+    ''' Writes jump information to the csv log
+    
+    :param file_name: the full path to the csv file
+           csv_list:  the 2d csv list to be written
+    :type file_name:  string
+          csv_list:   list of strings
+    :rtype: N/A
+    '''
     with open(file_name, 'w') as outfile:
         csv_writer = writer(outfile)
         for row in csv_list:
@@ -67,7 +89,14 @@ def save_csv_file(file_name, csv_list):
 #######################################
 
 def get_total_jumps(log_book):
-    b = f = c = t = 0.0
+    ''' Returns the amount of belly, freefly, canopy jumps, and
+        estimated freefall time in the air.
+    
+    :param log_book: the entire log of the user's jumps
+    :type log_book:  2d list
+    :rtype: ints [belly, freefly, wingsuit, canopy jumps and total freefall]
+    '''
+    b = f = c = t = w = 0.0
     for item in log_book:
         if item[6].lower() == 'b':
             t += (int(item[2]) - 3000) * ( 1 / 12000)
@@ -77,10 +106,18 @@ def get_total_jumps(log_book):
             f += 1
         elif item[6].lower() == 'c':
             c += 1
-    return int(b), int(f), int(c), int(t)
+        elif item[6].lower() == 'w':
+            w += 1
+    return int(b), int(f), int(c), int(w), int(t)
      
 
 def get_date():
+    ''' Returns the date of jump inputted by user
+    
+    :param None:
+    :rtype: string
+    :rformat: 'xx/xx/xxxx'
+    '''
     while True:
         print('Enter date or press [t] for today')
         date = input('-> Date [mm/dd/yyyy]: ')
@@ -103,25 +140,47 @@ def get_date():
 
 
 def get_altitude():
+    ''' Returns the altitude inputted by the user.
+    
+    :param None:
+    :rtype: int
+    '''
     while True:
         altitude = input('-> Altitude: ')
-        altitude = altitude.replace(',', '')
+        altitude = altitude.replace(',', '').strip()
         
         try:
             altitude = int(altitude)
-            break
+            if altitude > 15000:
+                print(f'{altitude} feet seems a bit high. Is this correct? [Y/N]')
+                ans = input('-> ').strip().lower()
+                if ans == 'y':
+                    break
+            else:
+                break
         except:
             print('Invalid format. Cannot be converted to integer')
+            
     return altitude
 
 
 def get_location():
+    ''' Returns the location inputted by the user
+    
+    :param None:
+    :rtype: string
+    '''
     location = input('-> Location: ')
-    location = location.replace(',', '')
+    location = location.strip().replace(',', '')
     return location
         
 
 def get_aircraft():
+    ''' Returns the aircraft type selected by the user
+    
+    :param None:
+    :rtype: string
+    '''
     print('Type in aircraft or select from list below:')
     print('[1 - Otter, 2 - Caravan, 3 - King Air, 4 - Cessna 182]')
     aircraft = input('-> ')
@@ -140,6 +199,11 @@ def get_aircraft():
   
   
 def get_equipment():
+    ''' Returns the size of parachute inputted by the user.
+    
+    :param None:
+    :rtype: int
+    '''
     while True:
         try:
             equipment = int(input('-> Equipment: ').strip())
@@ -154,6 +218,12 @@ def get_equipment():
 
 
 def get_type_of_jump():
+    ''' Returns the type of jump inputted by user. Jump types include
+        belly jumps, freefly jumps, wingsuit jumps, or canopy.
+        
+    :param None:
+    :rtype: string/char
+    '''
     while True:
         print('[b - belly, f - freefly, w - wingsuit, c - canopy]')
         type = input('-> Type of jump: ').strip()
@@ -165,6 +235,13 @@ def get_type_of_jump():
     
 
 def get_signature():
+    ''' Returns the license number of another skydiver. Must
+        be of a specific format shown as :rformat:
+    
+    :param None:
+    :rtype: string
+    :rformat: '<A,B,C,orD>-<five digit number>' ex. 'C-12345'
+    '''
     while True:
         signature = input('-> Signature [ex. C-12345]: ').strip()
         if (len(signature) == 7) and \
@@ -181,11 +258,32 @@ def get_signature():
         
 
 def get_notes():
-    note = input('-> Description: ').replace(',','').strip()
+    ''' Returns the user's description of the jump. Character limit
+        set at 300.
+        
+    :param None:
+    :rtype: string
+    '''
+    while True:
+        note = input('-> Description: ').replace(',','').strip()
+        if len(note) > 300:
+            print('Cannot exceed 300 characters.')
+        else:
+            break
+        
     return note
     
     
 def add_jumps(log_book, skydiver_info):
+    ''' Driver for logging jumps and saving the new info into
+        the csv file / log book.
+    
+    :param log_book:      Entire log of the user's jumps
+           skydiver_info: Personal info about the user
+    type   log_book:      2d list
+           skydiver_info: list
+    :rtype: None
+    '''
     print('How many jumps?')
     while True:
         try:
