530d90a29d92157203e8708d46929fea317854c2
khurd21 <kyle.hurd@wsu.edu>
2020-12-28 01:24:45 -0800

    Editing Files

---
+++ edit.py
@@ -0,0 +1,22 @@
+#############################
+# Name:        Kyle Hurd
+# Project:     Virtual skydiving log book.
+# Description: This file controls the content of log.csv and
+#              info.txt, updating the information when requested
+#              by the user and ensuring the files do not become
+#              corrupted.
+############################
+
+
+# Modules
+############################
+import display as d
+
+
+# Functions
+############################
+
+
+def edit_home():
+    pass
+    
